8eed971ff74cd0d1e0a8127234c11e357813aa73
Jacek Markowski <jacek87markowski@gmail.com>
2015-11-07 19:51:46 +0100

    fltk succesfull compilation, simple working gui app

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,4 +1,13 @@
-module Main (main) where
+module Main where
+import MainFLTK
+import Graphics.UI.FLTK.LowLevel.FLTKHS
+import qualified Graphics.UI.FLTK.LowLevel.FL as FL
+import Graphics.UI.FLTK.LowLevel.Fl_Types
+import Graphics.UI.FLTK.LowLevel.Fl_Enumerations
 
-main :: IO ()
-main = undefined
+main :: IO()
+main = do
+  window <- make_window
+  _ <- showWidget window
+  _ <- FL.run
+  return ()
