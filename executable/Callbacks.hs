26c8f53298f6a36008415d39e70828f8c5ce4aed
Jacek Markowski <jacek87markowski@gmail.com>
2015-11-16 01:09:15 +0100

    simple gui for displaying teams

--- executable/Callbacks.hs
+++ executable/Callbacks.hs
@@ -2,9 +2,15 @@ module Callbacks where
 import qualified Graphics.UI.FLTK.LowLevel.FL as FL
 import Graphics.UI.FLTK.LowLevel.Fl_Types
 import Graphics.UI.FLTK.LowLevel.FLTKHS
-buttonCb :: Ref Button -> IO ()
-buttonCb b' = do
-  l' <- getLabel b'
-  if (l' == "Hello World")
-    then setLabel b' "Goodbye World"
-    else setLabel b' "Hello World"
+
+import HsPredictor.Render
+import HsPredictor.LoadCSV
+
+renderTableCb :: Ref Output -> Ref Input -> Ref Button -> IO ()
+renderTableCb out inp _ = do
+  csv <- getValue inp
+  let db = csv ++ ".db"
+  loadCSV csv db
+  rTable <- renderTable db
+  setValue out rTable Nothing
+  return ()
