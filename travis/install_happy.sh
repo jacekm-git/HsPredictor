097e702f42baadd1fa3887e71c29bcf6af4bc2c6
Jacek Markowski <jacek87markowski@gmail.com>
2015-11-21 21:49:51 +0100

    travis happy install script

---
+++ travis/install_happy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+DIRECTORY=${HOME}
+FILE=happy
+PATH=${DIRECTORY}/.cabal/bin/
+FPATH="${PATH}${FILE}"
+if [ -e "$FPATH" ];
+then
+    echo "Happy exists. Do nothing"
+else
+    cabal install $FILE
